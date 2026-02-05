d76da964fe2e46cb4817042435085d59e5f37ce5
sohail <sksohail.swaraj@gmail.com>
2026-02-05 15:04:07 +0530

    small fix

--- brainlybackend/src/index.ts
+++ brainlybackend/src/index.ts
@@ -29,7 +29,7 @@ const user = await User.findOne({ username });
     if(!user){
       return res.json({message:"connot find the user"});
     }
-    const verify= await bcrypt.compare(password,user.password);
+    const verify= await bcrypt.compare(password,user.password!);
     if(!verify){
       return res.json({message:"wrong passsword"});
     }
